3763dd3634c90b611062c012b4f8dd7c5dfd2d27
Alex Acebo <aacebowork@gmail.com>
2026-03-18 07:23:01 -0700

    Update build.rs

--- crates/zynix/build.rs
+++ crates/zynix/build.rs
@@ -5,7 +5,7 @@ fn main() {
     if let Some((version, channel, _)) = rustc::triple() {
         if version.at_least("1.31.0") && channel.supports_features() {
             println!("cargo::rustc-check-cfg=cfg(nightly)");
-            println!("cargo:rustc-cfg=nightly");
+            println!("cargo::rustc-cfg=nightly");
         }
     }
 }
