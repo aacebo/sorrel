c0667540a6ca59eb945989541837d76142e7e590
Alex Acebo <aacebowork@gmail.com>
2026-03-23 07:15:28 -0700

    parser cursor

--- crates/zynix/src/parse/mod.rs
+++ crates/zynix/src/parse/mod.rs
@@ -1,9 +1,12 @@
+mod cursor;
 mod error;
 mod stream;
 
+#[allow(unused)]
+pub use cursor::*;
 pub use error::*;
 pub use stream::*;
 
 pub trait Parse: Sized {
-    fn parse(input: &mut ParseStream<'_>) -> Result<Self, ParseError>;
+    fn parse(stream: &mut ParseStream<'_>) -> Result<Self, ParseError>;
 }
